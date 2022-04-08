d0f9906b9ab5190cac0742d1a9ea3044af7e3628
gmaldona <gmaldona@oswego.edu>
2022-04-07 21:59:19 -0400

    tests

---
+++ app/src/test/java/F21/CSC385/Project1/GeneticAlgorithm/TestSuite.java
@@ -0,0 +1,14 @@
+package F21.CSC385.Project1.GeneticAlgorithm;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        FactoryFloorTest.class,
+        StationTest.class
+})
+
+public class TestSuite {
+}
